149e6b160ed5ae1ca6895bcdf3fdf711d06654a0
Anushka66 <anushkatrivedi0898@gmail.com>
2024-04-19 18:44:23 +0530

    delete button for creater

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,7 @@
 const { Router } = require("express");
 const multer = require("multer");
 const Blog = require("../models/blog");
-const Comment= require("../models/comment");
+const Comment = require("../models/comment");
 const path = require("path");
 const router = Router();
 
@@ -26,15 +26,15 @@ router.get("/add-new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
     const blog = await Blog.findById(req.params.id).populate("createdBy");
-    const comments= await Comment.find({ blogId: req.params.id}).populate("createdBy");
+    const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy");
     return res.render("blog", { user: req.user, blog, comments })
 });
 
-router.post('/comment/:blogId',async(req,res)=>{
+router.post('/comment/:blogId', async (req, res) => {
     await Comment.create({
-         content: req.body.content,
-         blogId: req.params.blogId,
-         createdBy: req.user._id,
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
     });
     return res.redirect(`/blog/${req.params.blogId}`);
 });
@@ -50,13 +50,14 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
     return res.redirect(`/blog/${blog._id}`);
 });
 
-router.post("/delete/:id", async(req,res)=>{
-   try {
-      await Blog.findByIdAndDelete(req.params.id);
-      return res.redirect("/", {user: req.user});
-   } catch (error) {
-    console.log(erroe);
-   }
+router.post("/delete/:id", async (req, res) => {
+    try {
+        console.log(req.params.id);
+        await Blog.findByIdAndDelete(req.params.id);
+        return res.redirect("/", { user: req.user });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
